2d2b8ec65e5aa1f786faf3270dfc07d406540b4d
Emiliano <2123100595@soy.utj.edu.mx>
2025-02-11 13:08:17 -0600

    SAQUEN SEXOSERVIDORAS

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
+
 use function response;
 
 class AdminAuthController extends Controller
--- app/Models/PersonalAccessToken.php
+++ app/Models/PersonalAccessToken.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Laravel\Sanctum\PersonalAccessToken as SanctumToken;
 use MongoDB\Laravel\Eloquent\DocumentModel;
+
 class PersonalAccessToken extends SanctumToken
 {
     use DocumentModel;
+
     protected $connection = 'mongodb';
-    protected $table = 'Aministradores';
+    protected $table = 'personal_access_tokens';
     protected $keyType = 'string';
 }
